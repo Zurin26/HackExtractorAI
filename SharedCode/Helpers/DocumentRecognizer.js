30846fada8c7098f19fbcab484af4d04a52776ea
JM <46513545+Zurin26@users.noreply.github.com>
2023-03-30 12:28:25 +0800

    listing all pages, word and content

--- SharedCode/Helpers/DocumentRecognizer.js
+++ SharedCode/Helpers/DocumentRecognizer.js
@@ -1,10 +1,5 @@
 const { DocumentAnalysisClient, AzureKeyCredential } = require("@azure/ai-form-recognizer");
 
-const fs = require("fs");
-const FileReader = require("filereader")
-const { v4: uuidv4 } = require("uuid");
-const { request } = require("https");
-
 const FR_URL = process.env.FR_URL
 const FR_KEY = process.env.FR_KEY
 
@@ -15,7 +10,6 @@ function* getTextOfSpans(content, spans) {
   }
 
 async function readDocumentData(urlfile) {
-    // const modelID = uuidv4();
     const formUrl = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/rest-api/read.png"
     const client = new DocumentAnalysisClient(
         FR_URL,
@@ -26,62 +20,58 @@ async function readDocumentData(urlfile) {
 
     const { content, languages, pages, styles } = await poller.pollUntilDone();
 
-    // if (pages.length <= 0) {
-    //     console.log("No pages were extracted from the document.");
-    //   } else {
-    //     console.log("Pages:");
-    //     for (const page of pages) {
-    //       console.log("- Page", page.pageNumber, `(unit: ${page.unit})`);
-    //       console.log(`  ${page.width}x${page.height}, angle: ${page.angle}`);
-    //       console.log(`  ${page.lines.length} lines, ${page.words.length} words`);
-    
-    //       if (page.lines.length > 0) {
-    //         console.log("  Lines:");
-    
-    //         for (const line of page.lines) {
-    //           console.log(`  - "${line.content}"`);
-    //           for (const word of line.words()) {
-    //             console.log(`    - "${word.content}"`);
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
+    const pagesLine = [];
+    const pagesWord = [];
+    const contentLang = [];
+    const contentStyle = [];
 
-    //   if (languages.length <= 0) {
-    //     console.log("No language spans were extracted from the document.");
-    //   } else {
-    //     console.log("Languages:");
-    //     for (const languageEntry of languages) {
-    //       console.log(
-    //         `- Found language: ${languageEntry.languageCode} (confidence: ${languageEntry.confidence})`
-    //       );
-    //       for (const text of getTextOfSpans(content, languageEntry.spans)) {
-    //         const escapedText = text.replace(/\r?\n/g, "\\n").replace(/"/g, '\\"');
-    //         console.log(`  - "${escapedText}"`);
-    //       }
-    //     }
-    //   }
+    if (pages.length <= 0) {
+      } else {
+        for (const page of pages) {
+          if (page.lines.length > 0) {
+            for (const line of page.lines) {
+            //   console.log(`  - "${line.content}"`);
+              pagesLine.push(line.content);
+              for (const word of line.words()) {
+                // console.log(`    - "${word.content}"`);
+                pagesWord.push(word.content);
+              }
+            }
+          }
+        }
+      }
     
-    //   if (styles.length <= 0) {
-    //     console.log("No text styles were extracted from the document.");
-    //   } else {
-    //     console.log("Styles:");
-    //     for (const style of styles) {
-    //       console.log(
-    //         `- Handwritten: ${style.isHandwritten ? "yes" : "no"} (confidence=${style.confidence})`
-    //       );
+      if (languages.length <= 0) {
+      } else {
+        for (const languageEntry of languages) {
+          for (const text of getTextOfSpans(content, languageEntry.spans)) {
+            const escapedText = text.replace(/\r?\n/g, "\\n").replace(/"/g, '\\"');
+            // console.log(`  - "${escapedText}"`);
+            contentLang.push(escapedText);
+          }
+        }
+      }
     
-    //       for (const word of getTextOfSpans(content, style.spans)) {
-    //         console.log(`  - "${word}"`);
-    //       }
-    //     }
-    //   }
+      if (styles.length <= 0) {
+      } else {
+        for (const style of styles) {
+          for (const word of getTextOfSpans(content, style.spans)) {
+            // console.log(`  - "${word}"`);
+            contentStyle.push(word);
+          }
+        }
+      }
     
     return {
-        pages : pages,
+        pages : {
+            pagesLine : pagesLine,
+            pagesWord : pagesWord
+        },
         styles : styles,
-        content : content,
+        content : {
+            contentLang : contentLang,
+            contentStyle : contentStyle
+        },
         languages : languages
     }
 }
